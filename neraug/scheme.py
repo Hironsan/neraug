c33a324c2a54c4d8262427610008c8f04baadaf8
Hironsan <light.tree.1.13@gmail.com>
2021-07-21 15:33:11 +0900

    Add tagger

--- neraug/scheme.py
+++ neraug/scheme.py
@@ -1,7 +1,46 @@
-from seqeval import scheme
+import abc
+from typing import List, Type
 
-IOB2 = scheme.IOB2
-BILOU = scheme.BILOU
-IOBES = scheme.IOBES
-Token = scheme.Token
-Entities = scheme.Entities
+from seqeval import scheme as s
+
+IOB2 = s.IOB2
+BILOU = s.BILOU
+IOBES = s.IOBES
+Token = s.Token
+Entities = s.Entities
+
+
+def create_tagger(scheme: Type[Token]):
+    if scheme == IOB2:
+        return IOB2Tagger()
+    elif scheme == IOBES:
+        return StartInsideEndTagger()
+    elif scheme == BILOU:
+        return StartInsideEndTagger("U", "L")
+    else:
+        raise ValueError(f"The scheme value is invalid: {scheme}")
+
+
+class BaseTagger(abc.ABC):
+    @abc.abstractmethod
+    def tag(self, words: List[str], label: str, scheme: Type[Token]):
+        assert len(words) > 0
+
+
+class IOB2Tagger(BaseTagger):
+    def tag(self, words: List[str], label: str, scheme: Type[Token]):
+        super().tag(words, label, scheme)
+        return [f"B-{label}"] + [f"I-{label}"] * (len(words) - 1)
+
+
+class StartInsideEndTagger(BaseTagger):
+    def __init__(self, single="S", end="E"):
+        self.single = single
+        self.end = end
+
+    def tag(self, words: List[str], label: str, scheme: Type[Token]):
+        super().tag(words, label, scheme)
+        if len(words) == 1:
+            return [f"{self.single}-{label}"]
+        else:
+            return [f"B-{label}"] + [f"I-{label}"] * (len(words) - 2) + [f"{self.end}-{label}"]
